f5ab7fd1f439b6e5314c19e1abb69ea4e4498ddc
JacobRishe <Jacob.Rishe@gmail.com>
2021-05-12 15:08:54 -0400

    imported pages and added route with component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header';
 import Footer from './components/Footer/Footer';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 
-import {BrowserRouter as Router, Switch} from "react-router-dom"
+
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 function App() {
   return (
     <Router>
-    <Switch>
     <div className="main-container">
     <div className="content-wrap">
       <Navbar />
@@ -16,6 +19,10 @@ function App() {
     </div>
       <Footer />
     </div>
+    <Switch>
+      <Route path='/projects' component = {Projects} />
+      <Route path='/contact' component = {Contact} />
+      <Route path='/' component = {Home} />
     </Switch>
     </Router>
   );
