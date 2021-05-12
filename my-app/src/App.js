0ca41cfb482ef55c3016d2f27076cfd2ea11863f
JacobRishe <Jacob.Rishe@gmail.com>
2021-05-12 14:57:00 -0400

    added Switch

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,12 @@ import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header';
 import Footer from './components/Footer/Footer';
 
-import {BrowserRouter as Router} from "react-router-dom"
+import {BrowserRouter as Router, Switch} from "react-router-dom"
 
 function App() {
   return (
     <Router>
+    <Switch>
     <div className="main-container">
     <div className="content-wrap">
       <Navbar />
@@ -15,6 +16,7 @@ function App() {
     </div>
       <Footer />
     </div>
+    </Switch>
     </Router>
   );
 }
