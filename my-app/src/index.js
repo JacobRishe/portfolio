1b1aa47dd3fb5831c27ee66ac7edd47e633b010a
JacobRishe <Jacob.Rishe@gmail.com>
2021-05-25 16:05:39 -0700

    added aeventlistener for splash page

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from "react-router-dom"
+import { BrowserRouter as Router } from "react-router-dom"
 
 import './index.css';
 import App from './App';
 
 
+const splash = document.querySelector('.splash');
 
+document.addEventListener('DOMContentLoaded', (e) => {
+  setTimeout(() => {
+    splash.classList.add('display-none');
+  }, 2000);
+})
 
 ReactDOM.render(
   <React.StrictMode>
