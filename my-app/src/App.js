f78ff88d046ab7285a98bad3e97b2e006d4df54e
JacobRishe <Jacob.Rishe@gmail.com>
2021-06-08 01:22:10 -0700

    added path to about page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer/Footer';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
+import About from './pages/About';
 
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
@@ -15,6 +16,7 @@ function App() {
         <Switch>
           <Route path='/projects' component = { Projects } />
           <Route path='/contact' component = { Contact } />
+          <Route path='/about' component = { About } />
           <Route path='/' component = { Home } />
         </Switch>
         <Footer />
