73411b9b7f9e74bcc4fccf568c7ca4f24ffce778
JacobRishe <Jacob.Rishe@gmail.com>
2021-07-27 11:58:45 -0700

    added ContactCard component

--- my-app/src/pages/Contact.js
+++ my-app/src/pages/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import ContactCard from '../components/Contact/ContactCard.js'
 
 export default function Contact() {
     return (
         <div>
-            <h1>this is the contact page</h1>
+            <ContactCard />
         </div>
     )
 }
