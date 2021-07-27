8d2c4f687b6477735f6521d8c14bd18d468c9f45
JacobRishe <Jacob.Rishe@gmail.com>
2021-07-27 11:03:45 -0700

    added new skills component folder

---
+++ my-app/src/components/Skills/SkillsCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Skills.css'
+
+export default function SkillsCard({ title }) {
+    return (
+        <div>
+            <h1 className='skills-card'>{ title }</h1>
+        </div>
+    )
+}
