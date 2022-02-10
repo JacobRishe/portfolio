760a3e08c1910ac196006f048faf9281230f2c38
JacobRishe <Jacob.Rishe@gmail.com>
2022-02-10 11:59:10 -0800

    Made cards responsive and styled with flexbox.

--- my-app/src/components/Project/ProjectCard.js
+++ my-app/src/components/Project/ProjectCard.js
@@ -3,20 +3,22 @@ import './Project.css'
 
 export default function ProjectCard({title, photo, body, link}) {
     return (
-        <div className="card-container">
-            <a className="card-link" href={ link }>
-                <div className="image-container">
-                    <img src={ photo } alt='' />
-                </div>
-                <div className="card-content">
-                    <div className="card-body">
-                        <p className="card-paragraph">{ body }</p>
+        <div className="border">
+            <div className="card-container">
+                <a className="card-link" href={ link }>
+                    <div className="image-container">
+                        <img src={ photo } alt='' />
                     </div>
-                    <div className="card-title">
-                        <h3 className="card-title">{ title }</h3>
+                    <div className="card-content">
+                        <div className="card-body">
+                            <p className="card-paragraph">{ body }</p>
+                        </div>
+                        <div className="card-title">
+                            <h3 className="card-title">{ title }</h3>
+                        </div>
                     </div>
-                </div>
-            </a>
+                </a>
+            </div>
         </div>
     )
 }
