ebc2d7430a5933205b93b05132f7f59290f450b3
JacobRishe <Jacob.Rishe@gmail.com>
2022-02-17 11:16:40 -0800

    moved navbar to right side. Styled each link and used transition to fill color.

--- my-app/src/components/Navbar/Navbar.js
+++ my-app/src/components/Navbar/Navbar.js
@@ -11,31 +11,38 @@ class Navbar extends Component {
     }
 
     render() {
-        return <nav>
-            <div>
-                <h1 className="navbar-logo">J<span className="letter-r">R</span></h1>
-            </div>
-            <ul className="navLinks">
-                <li className="link"><a href="#">About</a></li>
-                <li className="link"><a href="#">Projects</a></li>
-                <li className="link"><a href="#">Skills</a></li>
-                <li className="link"><a href="#">Contact</a></li>
-            </ul>
-            <div className="iconWrapper" onClick={ this.handleClick }>
-                <i className={ this.state.clicked ? 'fas fa-times' : 'fas fa-bars' }></i>
-            </div>
-            <ul className={ this.state.clicked ? 'nav-menu active' : 'nav-menu' }>
-                { MenuItems.map((item, index) => {
-                    return(
-                        <li className="links" key={ index }>
-                            <a className={ item.cName } href={ item.url }>
-                            { item.title }
-                            </a>
-                        </li>
-                    )
-                })}
-            </ul>
-        </nav>
+        return (
+            <nav>
+                <ul className="navLinks">
+                    <div class="linkBackground">
+                        <li className="link"><a href="#">About</a></li>
+                    </div>  
+                    <div class="linkBackground">
+                        <li className="link"><a href="#">Projects</a></li>
+                    </div>  
+                    <div class="linkBackground">
+                        <li className="link"><a href="#">Skills</a></li>
+                    </div>  
+                    <div class="linkBackground">
+                        <li className="link"><a href="#">Contact</a></li>
+                    </div>  
+                </ul>
+                <div className="iconWrapper" onClick={ this.handleClick }>
+                    <i className={ this.state.clicked ? 'fas fa-times' : 'fas fa-bars' }></i>
+                </div>
+                <ul className={ this.state.clicked ? 'nav-menu active' : 'nav-menu' }>
+                    { MenuItems.map((item, index) => {
+                        return(
+                            <li className="links" key={ index }>
+                                <a className={ item.cName } href={ item.url }>
+                                { item.title }
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        )
         
     }
 }
