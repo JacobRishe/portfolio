443de199f69bc0f3931424795a3f664b39ed77ba
JacobRishe <Jacob.Rishe@gmail.com>
2022-02-18 14:18:14 -0800

    Re-styled and laid out component.

--- my-app/src/components/Skills/SkillSection.js
+++ my-app/src/components/Skills/SkillSection.js
@@ -1,5 +1,33 @@
 import React from 'react';
+import SkillsCard from './SkillsCard'
+import { FaGithub } from 'react-icons/fa'
 
 export default function SkillSection() {
-  return <div>skills section</div>;
+  return (
+    <div className="skillsContainer">
+			<h1 className="h1Text">MY</h1>
+			<h2 className="h2Text">SKILLS</h2>
+			<div className="skillsCardWrapper">
+				<SkillsCard
+					title='JavaScript'
+					icon={<FaGithub/>}
+				/>
+				<SkillsCard
+					title='React'
+				/>
+				<SkillsCard
+					title='Html'
+				/>
+				<SkillsCard
+					title='Css'
+				/>
+				<SkillsCard
+					title='Figma'
+				/>
+				<SkillsCard
+					title='Wordpress'
+				/>
+			</div>
+		</div>
+    )
 }
