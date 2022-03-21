8f8f9bc273cc0aafc7b7795ac3ce88fd5420f5e0
JacobRishe <Jacob.Rishe@gmail.com>
2022-03-21 12:55:55 -0700

    restyled to show on desktop but not mobile.

--- my-app/src/components/Footer/Footer.js
+++ my-app/src/components/Footer/Footer.js
@@ -11,27 +11,29 @@ export default function Footer() {
                     <div className="col">
                         <h3 className="header-title">CONTACT</h3>
                         <ul className="list-style">
-                            <li>315-276-6931</li>
-                            <li>Jacob.Rishe@gmail.com</li>
-                            <li>Culver City, CA</li>
+                            <li><a href="tel:3152766931">315-276-6931</a></li>
+                            <li><a href="mailto:Jacob.Rishe@gmail.com">Jacob.Rishe@gmail.com</a></li>
+                            <li><a href="https://www.google.com/maps/place/Los+Angeles,+CA/@34.0201613,-118.6919205,10z/data=!3m1!4b1!4m5!3m4!1s0x80c2c75ddc27da13:0xe22fdf6f254608f4!8m2!3d34.0522342!4d-118.2436849">Los Angeles, CA</a></li>
                         </ul>
                     </div>
                     {/* column2 */}
                     <div className="col">
                         <h3 className="header-title">SOCIAL</h3>
-                        <ul className="list-style">
-                            <li><a href="https://linkedin.com/in/JacobRishe">LinkedIn</a></li>
-                            <li><a href="https://github.com/JacobRishe">Github</a></li>
-                            <li><a href="https://codepen.io/jacob-rishe">Codepen</a></li>
-                        </ul>
+                        <div>
+                            <ul className="list-style">
+                                <li><a href="https://docs.google.com/document/d/1M-Do-dsSSRIyJCyJ8DAiDCbeG7IKQSksx9r2mxSAT0w/edit?usp=sharing">Sites Built</a></li>
+                                <li><a href="https://linkedin.com/in/JacobRishe">LinkedIn</a></li>
+                                <li><a href="https://github.com/JacobRishe">Github</a></li>
+                            </ul>
+                        </div>
                     </div>
                     {/* column3 */}
                     <div className="col">
-                        <h3 className="header-title">CONTACT</h3>
+                        <h3 className="header-title">PROJECTS</h3>
                         <ul className="list-style">
-                            <li>315-276-6931</li>
-                            <li>Jacob.Rishe@gmail.com</li>
-                            <li>Culver City, CA</li>
+                            <li><a href="https://github.com/JacobRishe">MERN STACK</a></li>
+                            <li><a href="https://wayfarer-sei.herokuapp.com/">Python APP</a></li>
+                            <li><a href="https://github.com/JacobRishe/API_APP">API APP</a></li>
                         </ul>
                     </div>
                 </div>
